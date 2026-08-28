d4eb41fe301ee122f5db4fab8b9fc089596718d5
Aaron Loera <134811971+Aaron-Loera@users.noreply.github.com>
2026-08-28 11:17:36 -0500

    Implement ingredient selection from fruit_options
    Add multiselect for choosing fruit ingredients from Snowflake table.

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,8 @@ st.write("The name of your Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
+
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
     my_dataframe,
